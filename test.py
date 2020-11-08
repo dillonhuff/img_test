58fb3c5b98d9850533bd61d63292d6f0ada40d52
Dillon Huff <dillonhuff@gmail.com>
2020-11-07 16:48:12 -0800

    Can now write np array as color!

--- test.py
+++ test.py
@@ -31,9 +31,9 @@ cap.release()
 sf = 640
 ef = 480
 num_frames = buf.shape[0]
-writer = cv2.VideoWriter('gray_head.mp4', cv2.VideoWriter_fourcc(*'ffds'), 25, (sf, ef), False)
+writer = cv2.VideoWriter('gray_head.mp4', cv2.VideoWriter_fourcc(*'ffds'), 25, (sf, ef), True)
 for i in range(num_frames):
-    frame = buf[i, 0:480, 0:640, 0]
+    frame = buf[i, 0:480, 0:640]
     print('frame shape =', frame.shape)
     # x = np.random.randint(255, size=(ef, sf)).astype('uint8')
     x = frame.astype('uint8')
