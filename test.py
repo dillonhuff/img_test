647eb3631b4e9c7ae363944d73fbe64fb13de619
Dillon Huff <dillonhuff@gmail.com>
2020-11-07 16:41:25 -0800

    changed length of encoding

--- test.py
+++ test.py
@@ -30,7 +30,7 @@ cap.release()
 
 sf = 640
 writer = cv2.VideoWriter('gray_head.mp4', cv2.VideoWriter_fourcc(*'ffds'), 25, (sf, 480), False)
-for i in range(100):
+for i in range(20):
     x = np.random.randint(255, size=(480, sf)).astype('uint8')
     writer.write(x)
 
